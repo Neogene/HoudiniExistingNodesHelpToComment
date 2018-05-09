e5cec06a997e2611121c41a34732bbdd3553c265
Neogene <neogene@gmail.com>
2018-05-09 08:38:29 +0200

    First release

---
+++ OnUpdated.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+#TILE: HOUDINI HELP TO COMMENT
+#AUTHOR: eng. ANDREA LEGANZA
+#HOUDINI VERSION: TESTED ON HOUDINI 16.0 AND 16.5
+#SCRIPT VERSION: 1.0
+#DESCRIPTION:
+# 1) place script inside a Node->Right Mouse Button -> Type Properties -> scripts
+# 2) script will add for any preexistent children node as comment the headline taken from online documentation
+# NOTE: some scripts don't have headline or entire documentation 
+
+import hou
+import os
+import zipfile
+
+ZIPFOLDER = os.environ['HFS']+"/houdini/help/nodes.zip".replace("/",os.sep)
+ARCHIVE = zipfile.ZipFile(ZIPFOLDER, 'r')
+   
+def getHeader(path):
+    path = path.lower()
+    path = path.replace("operator:","").replace("object/","obj/").split("?")[0]
+   
+    if "invalid" in path:
+        return ""
+    
+    try:
+        nodeHelpContent = ARCHIVE.read(path+".txt")
+        splitted = nodeHelpContent.split("\"\"\"")
+        return splitted[1] if len(splitted)>1 else "Not found"
+    except:
+        return "Not found"
+
+def iterateChildren(node):
+     for child in node.children():
+ 
+        if len(child.comment())==0 :
+            description = getHeader(child.type().defaultHelpUrl())
+            child.setComment(description)
+            child.setGenericFlag(hou.nodeFlag.DisplayComment,True)
+            
+        if child.isLockedHDA(): 
+            hou.ui.displayMessage("LOCKED: "+child.name())
+            return
+        elif child.children()>0 :
+            iterateChildren(child)
+                
+    
+
+def main():
+    iterateChildren(hou.node("/"))
+
+main()
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # HoudiniExistingNodesHelpToComment
-A PYTHON script which populates existing node comments with their headline documentation
+A script which auto populate PREEXISTENT nodes comment with online documentation headline
+
+## AUTHOR: ## 
+eng. ANDREA LEGANZA
+
+## TESTED VERSION/S: ## 
+HOUDINI 16/16.5
+
+## DESCRIPTION: ## 
+When learning Houdini it may be hard to undestand the flow of nodes and to take a sneak peak on the purpose of a single node: a user has to select right click on node -> help and wait for Houdini internal browser to load the help page. This script simply read node headline (the title) from the internal documentation ad uses to populate ALL node comment.
+
+## RESULTS: ##
+![Script result](https://github.com/Neogene/HoudiniExistingNodesHelpToComment/blob/master/before_after.png)
+
+## INSTALLATION: ##
+1. place script inside a Geo/Node->Right Mouse Button -> Type Properties -> scripts -> OnUpdate (remember to select PYTHON on bottom right dropdown menu)
+2. script will add for any preexistent children nodes as comment the headline taken from online documentation
+
+## NOTE: ## 
+Some nodes don't have headline or the whole documentation file.
+
+## CONTRIBUTING: ##
+Please feel free to optimize the script and add/report missing nodes documentation. 
